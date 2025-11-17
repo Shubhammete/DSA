f3281ee9dd0a350e851e98c1be34e26079a83960
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2025-11-17 01:20:08 +0000

    Basic input output

---
+++ C++ DSA/Basics/io.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h> // has all standard libraries
+
+using namespace std;
+
+int main(){
+
+    // output
+    cout << "Hello" << endl << "Shubham!" << "\n";   // endl and \n is used to print on next line
+
+    // input
+    int a;
+    cout << "Enter value of a";
+    cin >> a;   // input value in variable
+    cout << "Value of a is " << a;
+
+    return 0;
+
+
+}
