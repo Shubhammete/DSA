1a959ffe49033466a32b8b82be26adbf277c50f5
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2025-11-17 01:30:18 +0000

    Data types

---
+++ C++ DSA/Basics/dataTypes.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    // Integer
+    int a = 10;
+    long b = 100000;
+    long long c = 334923942242;
+
+    // Decimal
+    float d = 2.5;
+    double e = 2.3444
+
+    // String
+    string f , g;
+    cin >> f >> g; // input = shubham mete >>>> f = shubham and g = mete
+    cout << "full name " << f << " "<< g; 
+    string one;
+    getline(cin, one);
+    cout << "Full Name " << one; 
+
+    // char
+    char h = 'g' // String is denoted in "" and char in ''
+    
+
+    return 0;
+}
