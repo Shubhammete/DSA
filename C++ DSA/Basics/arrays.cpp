b9f5da6941dc6234a71487351d417eba193c0713
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2025-11-17 02:44:16 +0000

    arrays basic

---
+++ C++ DSA/Basics/arrays.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    // decelaration
+    int arr[5];
+    // input one char
+    cin >> arr[1];
+    // input all elements
+    int size = 5
+    for (int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+
+    // 2D array
+    int arrr[3][5];  // 3 rows and 5 columns
+    
+
+    return 0;
+}
