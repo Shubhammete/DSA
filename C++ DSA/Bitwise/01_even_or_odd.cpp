70fe8c6b3e4d7fe10c33e1380648850fddc312e2
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2026-04-21 08:38:15 +0530

    Add program to check if a number is even or odd

---
+++ C++ DSA/Bitwise/01_even_or_odd.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int no = 522231;
+    if((no & 1) == 0){
+        cout << "Even";
+    }else{
+        cout<<"Odd";
+    }
+    return 0;
+}
