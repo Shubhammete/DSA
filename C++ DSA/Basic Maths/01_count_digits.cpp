1887ca31eb3496dc2196e56085bacd0b60743524
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2026-04-21 08:33:31 +0530

    Implement digit counting function in C++

--- C++ DSA/Basic Maths/01_count_digits.cpp
+++ C++ DSA/Basic Maths/01_count_digits.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+int checkdigits(int no, int ans){
+    while(no>0){
+        no = no/10;
+        ans = ans + 1;
+    }
+    return ans;
+}
+int main() {
+    int no = 522234;
+    int ans = 0;
+    cout << checkdigits(no, ans);
 
+    return 0;
+}
